cb6b2ba9d889961a314e0a184c8108159b0fd756
Sebastian Aviles <sebastian.aviles@codemaker.cl>
2018-04-24 13:55:31 -0300

    Fix animal breeding (Cow and Sheep)

--- src/main/java/com/pam/harvestcraft/events/AnimalBreedingEvent.java
+++ src/main/java/com/pam/harvestcraft/events/AnimalBreedingEvent.java
@@ -1,6 +1,5 @@
 package com.pam.harvestcraft.events;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -8,10 +7,12 @@ import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.oredict.OreDictionary;
@@ -21,17 +22,18 @@ public class AnimalBreedingEvent {
 	@SubscribeEvent
 	public void onInteraction(PlayerInteractEvent.EntityInteract event) {
 		Set<Item> breedItems = OreDictionary.getOres("listAllgrain").stream().map(ItemStack::getItem).collect(Collectors.toSet());
-		
-		if (event.getEntity() instanceof EntityCow) {
-	        handleBreedItem((EntityCow) event.getEntity(), breedItems, event.getEntityPlayer(), event.getHand());
-	    } else if (event.getEntity() instanceof EntitySheep) {
-	        handleBreedItem((EntityCow) event.getEntity(), breedItems, event.getEntityPlayer(), event.getHand());
+		Entity target = event.getTarget();
+		if (target instanceof EntityCow) {
+	        handleBreedItem((EntityCow) target, breedItems, event.getEntityPlayer(), event.getHand());
+	    } else if (target instanceof EntitySheep) {
+	        handleBreedItem((EntitySheep) target, breedItems, event.getEntityPlayer(), event.getHand());
 	    }
+		
 	}
+		
 
 	private void handleBreedItem(EntityAnimal entity, Set<Item> breedItems, EntityPlayer player, EnumHand hand) {
 		ItemStack itemstack = player.getHeldItem(hand);
-
         if (!itemstack.isEmpty())
         {
         	int inLove = ReflectionHelper.getPrivateValue(EntityAnimal.class, entity, "inLove", "field_70881_d");
