ff993117dfd5e13ae1a222782127fee9aeb881bc
temp1011 <34900092+temp1011@users.noreply.github.com>
2018-08-09 22:44:09 +0100

    use non deprecated getdrops closes #53

--- src/main/java/com/pam/harvestcraft/blocks/growables/PamCropGrowable.java
+++ src/main/java/com/pam/harvestcraft/blocks/growables/PamCropGrowable.java
@@ -2,18 +2,25 @@ package com.pam.harvestcraft.blocks.growables;
 
 import java.util.List;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public interface PamCropGrowable {
     int getMatureAge();
     boolean isMature(IBlockState state);
-    List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune);
+    default List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        NonNullList<ItemStack> ret = NonNullList.create();
+        getDrops(ret, world, pos, state, fortune);
+        return ret;
+    }
+
+    void getDrops(NonNullList<ItemStack> ret, IBlockAccess world, BlockPos pos,IBlockState state, int fortune);
+
+
     PropertyInteger getAgeProperty();
     
     
