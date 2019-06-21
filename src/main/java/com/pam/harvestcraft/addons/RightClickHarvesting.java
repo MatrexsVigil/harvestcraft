203a307d1f120787f90ee74e18a37a6e0181cc8b
ImMorpheus <ImMorpheus@users.noreply.github.com>
2019-06-21 20:43:13 +0200

    Fix issue with mods canceling PlayerInteractEvent.RightClickBlock

--- src/main/java/com/pam/harvestcraft/addons/RightClickHarvesting.java
+++ src/main/java/com/pam/harvestcraft/addons/RightClickHarvesting.java
@@ -47,6 +47,9 @@ public class RightClickHarvesting {
 		if(!HarvestCraft.config.enableEasyHarvest)
 			return;
 
+		if(event.isCanceled())
+			return;
+
 		if(event.getEntityPlayer() == null)
 			return;
 
